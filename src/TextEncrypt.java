480aa6c08a75c3f45c0605b4b6b363726cdd5328
axc450 <axc450@cs.bham.ac.uk>
2017-09-10 20:57:39 +0200

    Added Text Encryption (not yet working)

---
+++ src/TextEncrypt.java
@@ -0,0 +1,67 @@
+import java.security.MessageDigest;
+import java.util.Random;
+
+public class TextEncrypt 
+{	
+	
+	Random textGenerator;
+	
+	public TextEncrypt(String key) 
+	{
+		try
+		{
+			MessageDigest md5Generator = MessageDigest.getInstance("MD5");				//Create a MD5 generator
+			byte[] md5Result = md5Generator.digest(key.getBytes());						//Create the hash based on the key
+		
+			long randomSeed = md5ToSeed(md5Result);										//Convert the MD5 bytes to a long
+			
+			textGenerator = new Random();										//Create new random instance
+			textGenerator.setSeed(randomSeed);										//Set the random seed to the MD5 long
+		}
+		catch(Exception e)
+	    {
+	    	System.out.println("Something went wrong setting up the encryptor");		//Debug message
+			System.out.println("Error: " + e.getMessage());							//Debug message
+			System.exit(1);															//Exit program
+	    }
+	}
+
+	public String encrypt(String inputText) 
+	{
+			try
+			{
+				String encryptedStr = "";
+				
+	            for (int i = 0; i < inputText.length(); i++) 						//Loop through all pixel columns
+	            {
+	                    char chr = inputText.charAt(i);								//Get the current pixel color at this location
+	                    char newChr = encryptChr(chr);								//Ecrypt the pixel
+	                    encryptedStr += newChr;										//Change the input image pixel at this location
+	            }
+	                
+	            CryptoVision.printPercent(100);											//Print encryption progress
+			
+	            return encryptedStr;											//Return the new image
+			}
+		    catch(Exception e)
+		    {
+		    	System.out.println("Something went wrong encrypting your text");		//Debug message
+				System.out.println("Error: " + e.getMessage());							//Debug message
+				System.exit(1);															//Exit program
+				return null;
+		    }
+	}
+	
+	private char encryptChr(char chr)
+	{
+		int randInt = textGenerator.nextInt(100);  
+		return (char) ((char) ((int) chr)^(randInt));
+	}
+	
+	private static long md5ToSeed(byte[] md5Result)		//Converts an MD5 hash into a seed
+	{
+		long a = md5Result[0] * 256 * md5Result[1] + 256 * 256 * md5Result[2] + 256 * 256 * 256 * md5Result[3];
+		long b = md5Result[4] * 256 * md5Result[5] + 256 * 256 * md5Result[6] + 256 * 256 * 256 * md5Result[7];
+		return a ^ b;
+	}
+}
